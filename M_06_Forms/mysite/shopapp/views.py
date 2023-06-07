1c0722a3b49c5925819273a0a5ddf5c221baf3eb
Vladimir Brando <ad.sychev22@gmail.com>
2023-06-07 10:37:47 +0300

    Homework done

--- M_06_Forms/mysite/shopapp/views.py
+++ M_06_Forms/mysite/shopapp/views.py
@@ -2,12 +2,14 @@ from timeit import default_timer
 
 from django.contrib.auth.models import Group
 from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 from .models import Product, Order
+from .forms import ProductForm, OrderForm
 
 
-def shop_index(request: HttpRequest):
+def shop_index(request: HttpRequest) -> HttpResponse:
     products = [
         ('Laptop', 1999),
         ('Desktop', 2999),
@@ -20,22 +22,54 @@ def shop_index(request: HttpRequest):
     return render(request, 'shopapp/shop-index.html', context=context)
 
 
-def groups_list(request: HttpRequest):
+def groups_list(request: HttpRequest) -> HttpResponse:
     context = {
         "groups": Group.objects.prefetch_related('permissions').all(),
     }
     return render(request, 'shopapp/groups-list.html', context=context)
 
 
-def products_list(request: HttpRequest):
+def products_list(request: HttpRequest) -> HttpResponse:
     context = {
         "products": Product.objects.all(),
     }
     return render(request, 'shopapp/products-list.html', context=context)
 
 
-def orders_list(request: HttpRequest):
+def orders_list(request: HttpRequest) -> HttpResponse:
     context = {
         "orders": Order.objects.select_related("user").prefetch_related("products").all(),
     }
     return render(request, 'shopapp/orders-list.html', context=context)
+
+
+def create_product(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            url = reverse('shopapp:products_list')
+            return redirect(url)
+    else:
+        form = ProductForm()
+    context = {
+        "form": form,
+    }
+    return render(request, 'shopapp/create-product.html', context=context)
+
+
+def create_order(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            url = reverse('shopapp:orders_list')
+            return redirect(url)
+    else:
+        form = OrderForm()
+    context = {
+        "form": form,
+    }
+    return render(request, 'shopapp/create-order.html', context=context)
+    
+    
