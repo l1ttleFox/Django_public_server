054c4e74375ca7ce0bf6bec0844aee17cafb6e00
Vladimir Brando <69694115+l1ttleFox@users.noreply.github.com>
2023-07-05 16:29:53 +0300

    Delete M_05_RequestsAndMiddlewares directory

--- M_05_RequestsAndMiddlewares/mysite/requestsapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RequestsappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'requestsapp'
--- M_05_RequestsAndMiddlewares/mysite/requestsapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import upload_file_view
-
-app_name = "requestsapp"
-
-urlpatterns = [
-    path("upload/", upload_file_view, name="upload-file")
-]
