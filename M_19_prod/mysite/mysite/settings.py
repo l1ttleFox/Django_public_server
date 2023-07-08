3b8aa63ccdb578a999ff7858c120c9da0256bc19
Aleksey <ad.sychev22@gmail.com>
2023-07-08 21:23:44 +0300

    hot fixes

--- M_19_prod/mysite/mysite/settings.py
+++ M_19_prod/mysite/mysite/settings.py
@@ -164,6 +164,7 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'uploads'
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
